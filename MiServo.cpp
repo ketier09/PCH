d70071d45a764f063b13f865d7a90a8eabe89065
ketier09 <kevinmojicac@gmail.com>
2025-09-18 15:15:22 -0500

    Update and rename Motor.cpp to MiServo.cpp

---
+++ MiServo.cpp
@@ -0,0 +1,24 @@
+#include "MiServo.h"
+
+// Cuando se crea un motor, hay que decirle en qué dos pines (cables)
+// del Arduino está conectado. Un motor necesita dos para poder girar
+// en ambos sentidos.
+MiServo::MiServo(byte p)
+  : pin1(p) {}
+
+// Esta función prepara los dos pines para que el Arduino
+// los use como "interruptores" de salida y así poder controlar el motor.
+void MiServo::set_up() {
+  servo.attach(pin, estados[0], estados[sizeof(estados)/sizeof(estados[0]]);  // El segundo cable también funcionará como salida
+}
+
+void MiServo::cambiar_estado(byte estado){
+  int n = sizeof(estados)/sizeof(estados[0]);
+  int m = 2*n;
+  estado = estado % m;
+  if(estado >= n){
+    estado = m-estado-2;
+  }
+  servo.write(estados[estado]);
+}
+
