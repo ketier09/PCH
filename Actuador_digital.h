4578f159ecbcdcf5e508666c3a9a6396aa69dd8e
ketier09 <kevinmojicac@gmail.com>
2025-09-18 14:15:11 -0500

    Update Actuador_digital.h

--- Actuador_digital.h
+++ Actuador_digital.h
@@ -2,11 +2,9 @@
 #include <Arduino.h>
 
 struct valvula_motobomba {
-  const byte valvula;
-  const byte motobomba;
+  const byte pin;
 
-  valvula_motobomba(byte v, byte m);
+  valvula_motobomba(byte p);
   void set_up();
   void ejecutar_orden(char comando);
-  void leer_orden();
 };
