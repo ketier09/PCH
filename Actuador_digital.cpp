38fb4666981f0a302dbebb688657189ebc811ee0
ketier09 <kevinmojicac@gmail.com>
2025-09-18 14:17:58 -0500

    Update Actuador_digital.cpp

--- Actuador_digital.cpp
+++ Actuador_digital.cpp
@@ -1,30 +1,16 @@
 #include "Actuador_digital.h"
 
 valvula_motobomba::valvula_motobomba(byte v, byte m)
-  : valvula(v), motobomba(m) {}
+  : pin(p) {}
 
 void valvula_motobomba::set_up(){
-  Serial.println(F("\nEnvía '1' para ACTIVAR la valvula y luego la bomba."));
-  Serial.println(F("\nEnvía '0' para DESACTIVAR la bomba y luego la valvula."));
-  pinMode(valvula, OUTPUT);
-  pinMode(motobomba, OUTPUT);
+  pinMode(pin, OUTPUT);
 }
 
 void valvula_motobomba::ejecutar_orden(char comando){
       if(comando == '1'){
-      digitalWrite(valvula,HIGH);
-      digitalWrite(motobomba,HIGH);
-      Serial.println(F("\nElectroválvula y motobomba ENCENDIDAS"));
+      digitalWrite(pin,HIGH);
     }else if(comando == '0'){
-      digitalWrite(valvula,LOW);
-      digitalWrite(motobomba,LOW);
-      Serial.println(F("\nElectroválvula y motobomba APAGADAS"));
+      digitalWrite(pin,LOW);
     }
-}
-
-void valvula_motobomba::leer_orden(){
-  if (Serial.available() > 0){
-    char comando = Serial.read();
-    ejecutar_orden(comando);
-  }
 }
