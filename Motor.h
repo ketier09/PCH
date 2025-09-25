ed126f7e121c4ab58cb461957c522351128a5e1d
ketier09 <kevinmojicac@gmail.com>
2025-09-25 12:24:04 -0500

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -7,12 +7,11 @@
 struct motor {
   const byte pin;   // Primer cable que controla el motor
   const int estados[4];
+  const int n;
 
   // Constructor:
   motor(byte p, int e1, int e2, int e3, int e4);
   Servo servo;
-
-  static constexpr int n = sizeof(estados) / sizeof(estados[0]);
 
   // Métodos (funciones que puede hacer el motor):
   void set_up();     // Prepara los pines del motor para poder usarlos
