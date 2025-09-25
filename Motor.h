bd5c54ca8b54f33e9d342911225469abf0ba603d
ketier09 <kevinmojicac@gmail.com>
2025-09-25 12:37:56 -0500

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -1,21 +1,13 @@
-#pragma once
-#include <Arduino.h>
-#include <Servo.h>
-
-// Aquí definimos un "molde" llamado motor.
-// Sirve para manejar un motor conectado al Arduino con dos cables de control.
 struct motor {
-  const byte pin;   // Primer cable que controla el motor
-  const int estados[4];
-  const int n;
+  static constexpr int n = 4;     // <-- CAMBIO: explícito y seguro
 
-  // Constructor:
+  const byte pin;
+  const int estados[4];
+
   motor(byte p, int e1, int e2, int e3, int e4);
   Servo servo;
 
-  // Métodos (funciones que puede hacer el motor):
-  void set_up();     // Prepara los pines del motor para poder usarlos
+  int estado = 0;
+  void set_up();
   void siguiente_estado();
 };
-
-
