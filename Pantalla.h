028d208ff9485b5da066ddbeb9e0430ce3ff99c4
ketier09 <kevinmojicac@gmail.com>
2025-09-26 07:22:19 -0500

    Update Pantalla.h

--- Pantalla.h
+++ Pantalla.h
@@ -4,26 +4,37 @@
 #include <Adafruit_ILI9341.h>
 #include <SPI.h>
 
+#include "Datos.h"   // trae enum Dato, struct dato y el arreglo data[]
+
 class PantallaCustom {
 public:
-    PantallaCustom(uint8_t cs, uint8_t dc, uint8_t rst);
+  // l1..l3 (izq), r1..r3 (der) son índices del enum Dato
+  PantallaCustom(uint8_t cs, uint8_t dc, uint8_t rst,
+                 Dato l1, Dato l2, Dato l3,
+                 Dato r1, Dato r2, Dato r3);
 
-    void set_up();
-    void actualizar();
+  void set_up();
+  // Llama esto en loop pasando el arreglo global 'data'
+  void actualizar(const dato data[]);
 
 private:
-    Adafruit_ILI9341 tft;
-    unsigned long tiempoAnterior;
-    static constexpr unsigned long intervalo = 10000; // 10s
+  Adafruit_ILI9341 tft;
+  unsigned long tiempoAnterior = 0;
+  static constexpr unsigned long intervalo = 10000; // redibujar títulos cada 10 s
 
-    // Datos principales
-    int dato1, dato2, dato3;
-    int dato4, dato5, dato6;
+  Dato idx[6];     // 0..2 izquierda, 3..5 derecha
 
-    // Variables secundarias
-    int var1, var2, var3;
-    int var4, var5, var6;
+  // layout
+  static constexpr int LX = 10;
+  static constexpr int RX = 160;
+  static constexpr int YT[3] = {20, 60, 100};   // y títulos
+  static constexpr int YV[3] = {40, 80, 120};   // y valores
+  static constexpr int W  = 140;
+  static constexpr int H  = 20;
 
-    void dibujarTitulos();
-    void mostrarVariables();
+  void dibujarTitulos(const dato data[]);
+  void mostrarValores(const dato data[]);
+  void borrarCajaValor(bool derecha, int fila);
+  // imprime valor con formato: 2 decimales salvo unidades "    " (entero)
+  void imprimirValor(int x, int y, const dato& d);
 };
