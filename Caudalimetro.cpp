aea266ae1a9517f01700e06ace8f2f2bf0d84a6e
ketier09 <kevinmojicac@gmail.com>
2025-09-27 09:04:27 -0500

    Update Caudalimetro.cpp

--- Caudalimetro.cpp
+++ Caudalimetro.cpp
@@ -1,18 +1,14 @@
-// Caudalimetro.cpp
 #include "Caudalimetro.h"
 
 caudalimetro::caudalimetro(byte p) : pin(p) {}
 
 void caudalimetro::set_up() {
   pinMode(pin, INPUT);
-  // Usa attachInterruptArg para pasar "this" a la ISR
   attachInterruptArg(digitalPinToInterrupt(pin), &caudalimetro::isrThunk, this, FALLING);
 }
 
 void IRAM_ATTR caudalimetro::isrThunk(void* arg) {
   auto* self = static_cast<caudalimetro*>(arg);
-  // Incremento mínimo en ISR. No hace falta sección crítica aquí:
-  // la lectura ya se protege con portENTER_CRITICAL en reading().
   self->pulseCount++;
 }
 
@@ -27,9 +23,9 @@ float caudalimetro::reading() {
     pulseCount = 0;
     portEXIT_CRITICAL(&mux);
 
-    // m^3/s (pulsos / (pulsos/m^3 * s)) = m^3/s
     flowRate = pulses / FLOW_CALIBRATION_FACTOR;
   }
   return flowRate * ESCALA;
 }
+
 
