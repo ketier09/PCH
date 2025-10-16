59ffaa468ce4cc265ea5b7adebf5b7f59e114f1c
ketier09 <kevinmojicac@gmail.com>
2025-10-16 17:40:27 -0500

    Convert PantallaCustom from class to struct
    Refactor PantallaCustom class to struct and update interval.

--- PantallaCustom.h
+++ PantallaCustom.h
@@ -6,33 +6,26 @@
 
 #include "Datos.h"   // trae enum Dato, struct dato y el arreglo data[]
 
-class PantallaCustom {
-public:
+struct PantallaCustom {
+
+  static constexpr unsigned long intervalo = 5000;
+
+  Adafruit_ILI9341 tft;
+  Dato idx[6];     // 0..2 izquierda, 3..5 derecha
+  uint16_t colores[6] = {
+    ILI9341_RED, ILI9341_GREEN, ILI9341_BLUE,
+    ILI9341_YELLOW, ILI9341_CYAN, ILI9341_MAGENTA
+  };
+
   PantallaCustom(uint8_t cs, uint8_t dc, uint8_t rst,
                  Dato l1, Dato l2, Dato l3,
                  Dato r1, Dato r2, Dato r3);
 
+  uint32_t lastMillis = 0;
+  int indiceActual = 0;
+
+
   void set_up();
   void actualizar(const dato data[]);
-
-private:
-  Adafruit_ILI9341 tft;
-  unsigned long tiempoAnterior = 0;
-  static constexpr unsigned long intervalo = 10000; // redibujar títulos cada 10 s
-
-  Dato idx[6];     // 0..2 izquierda, 3..5 derecha
-
-  // layout
-  static constexpr int LX = 10;
-  static constexpr int RX = 160;
-  static constexpr int YT[3] = {20, 60, 100};   // y títulos
-  static constexpr int YV[3] = {40, 80, 120};   // y valores
-  static constexpr int W  = 140;
-  static constexpr int H  = 20;
-
-  void dibujarTitulos(const dato data[]);
-  void mostrarValores(const dato data[]);
-  void borrarCajaValor(bool derecha, int fila);
-  // imprime valor con formato: 2 decimales salvo unidades "    " (entero)
-  void imprimirValor(int x, int y, const dato& d);
 };
+
