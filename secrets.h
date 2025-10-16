c66b3cfe44257fb0b445b1bd021d3dc69e720e11
ketier09 <kevinmojicac@gmail.com>
2025-10-15 22:10:15 -0500

    reor

--- secrets.h
+++ secrets.h
@@ -1,7 +1,7 @@
 #pragma once
+extern const char WIFI_SSID[];
+extern const char WIFI_PASSWORD[];
 extern const char key[];
 extern const char url[];
 extern const char email[];
 extern const char password[];
-extern const char WIFI_SSID[];
-extern const char WIFI_PASSWORD[];
