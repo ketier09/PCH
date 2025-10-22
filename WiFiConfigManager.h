f6e9d87e67509f7352703c18413b7000cb5c303b
ketier09 <kevinmojicac@gmail.com>
2025-10-22 08:56:06 -0500

    Add WiFiConfigManager class for WiFi management

---
+++ WiFiConfigManager.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <LittleFS.h>
+
+class WiFiConfigManager {
+  public:
+    WiFiConfigManager(const char* path = "/wifi.json");
+    void begin();
+    void connect();
+    bool isConnected();
+
+  private:
+    String ssid;
+    String password;
+    const char* filePath;
+    WebServer server{80};
+
+    bool loadCredentials();
+    void saveCredentials(const String& ssid, const String& password);
+    void eraseCredentials();
+    void startConfigPortal();
+    void handleRoot();
+    void handleSave();
+};
