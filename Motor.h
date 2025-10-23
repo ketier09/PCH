c7a89f005d4a1fa930a72ecae95f8bbf7211a092
ketier09 <kevinmojicac@gmail.com>
2025-10-23 12:56:45 -0500

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -2,17 +2,20 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-struct motor {
+class motor {
+public:
   static constexpr int n = 4;
 
+  motor(byte p, int e1, int e2, int e3, int e4);
+
+  void set_up();
+  void siguiente_estado();
+
+private:
   const byte pin;
   const int estados[n];
-
-  motor(byte p, int e1, int e2, int e3, int e4);
 
   Servo servo;
 
   int estado = 0;
-  void set_up();
-  void siguiente_estado();
 };
