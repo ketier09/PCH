bf130e4e17dd531427802b0f07435d92200944d2
ketier09 <kevinmojicac@gmail.com>
2025-10-23 12:57:26 -0500

    Update Ultrasonico.h

--- Ultrasonico.h
+++ Ultrasonico.h
@@ -2,14 +2,25 @@
 #include <Arduino.h>
 #include <math.h>
 
-struct ultrasonico {
-  portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-
+class ultrasonico {
+public:
   static constexpr float kappa = 1.0f;
-  static constexpr float ESCALA = 1.0f; // m/cm
+  static constexpr float ESCALA = 1.0f; // m/cm (Se mantiene 1.0f para no romper el código, ver sugerencia 2)
   static constexpr float NIVEL_0 = 1268.0f;
   static constexpr float manningInverso = 1.0f / 0.013f;
   static constexpr float suavizador = 0.30f; //Para evitar transiciones drásticas
+
+  ultrasonico(byte t, byte e, int c, float te, float pi, float a, float pe);
+
+  void set_up();
+  void disparar();
+  float reading(uint32_t timeout_us = 30000); // ~5 m máx
+  float flujo();
+
+  static void IRAM_ATTR isrThunk(void* arg);
+
+private:
+  portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
   const byte trig;
   const byte echo;
@@ -19,20 +30,8 @@ struct ultrasonico {
   const float ancho;  // m
   const float raizCuadrada_pendiente; // = sqrt(S)
 
-  ultrasonico(byte t, byte e, int c, float te, float pi, float a, float pe);
-
   float nivel = NAN;
   float nivel_f = nivel;
   volatile uint32_t disparo = 0;
   volatile uint32_t duracion = 0;
-
-  void set_up();
-  void disparar();
-  float reading(uint32_t timeout_us = 30000); // ~5 m máx
-  float flujo();
-
-  static void IRAM_ATTR isrThunk(void* arg);
 };
-
-
-
