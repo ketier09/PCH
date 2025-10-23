b0e09dd2dfc9abf0c70ab84f44707fa5762fd17d
ketier09 <kevinmojicac@gmail.com>
2025-10-23 13:26:27 -0500

    Update Pulsador.h

--- Pulsador.h
+++ Pulsador.h
@@ -3,14 +3,14 @@
 
 class pulsador {
 public:
-  static constexpr unsigned long debounce = 50;
-
   pulsador(byte p, void (*i)(), bool pr);
 
   void set_up();
   void update();
 
 private:
+  static constexpr unsigned long debounce = 50;
+
   const byte pin;
   const bool pressed;
   void (*orden)();
