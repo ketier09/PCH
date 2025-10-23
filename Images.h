e67aaa318a9c0bb82e06a4595c1a444d76b3c4ac
ketier09 <kevinmojicac@gmail.com>
2025-10-23 14:41:15 -0500

    Update Images.h

--- Images.h
+++ Images.h
@@ -4,11 +4,13 @@
 
 class image {
 public:
-  constexpr image(const uint16_t* p, int w = 120, int h = 120)
-  : pixels(p), width(w), height(h) {}
+  constexpr image(const uint16_t* p, int x = 0, int y = 90, int w = 120, int h = 120)
+  : pixels(p), x_index(x), y_index(y), width(w), height(h) {}
 
 private:
   const uint16_t* pixels;
+  const x_index;
+  const y_index;
   const int width;
   const int height;
 };
