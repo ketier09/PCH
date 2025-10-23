4f5a195342fa68555edcc058171b4180abd175bc
ketier09 <kevinmojicac@gmail.com>
2025-10-23 13:25:37 -0500

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -4,14 +4,14 @@
 
 class motor {
 public:
-  static constexpr int n = 4;
-
   motor(byte p, int e1, int e2, int e3, int e4);
 
   void set_up();
   void siguiente_estado();
 
 private:
+  static constexpr int n = 4;
+
   const byte pin;
   const int estados[n];
 
