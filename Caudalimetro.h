2b3080780d4eb0ecbb98cdbf5f95d4b00a544444
ketier09 <kevinmojicac@gmail.com>
2025-10-23 13:14:49 -0500

    Update Caudalimetro.h

--- Caudalimetro.h
+++ Caudalimetro.h
@@ -13,13 +13,13 @@ public:
   static void IRAM_ATTR isrThunk(void* arg);
 
 private:
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 static constexpr float periodo_de_las_mediciones = 2000; // ms
   // 450 pulsos/L → 450000 pulsos/m^3. Resultado: m^3/s
   static constexpr float FLOW_CALIBRATION_FACTOR =
       450000.0f * (periodo_de_las_mediciones / 1000.0f);
 
   static constexpr float ESCALA = 100.0f; // Se mantiene por ser constante de diseño
-  portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
   const byte pin;
 
   uint32_t lastMillis = 0;
