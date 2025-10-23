69f744105d20120c0256ec20a39ad568986f7de3
ketier09 <kevinmojicac@gmail.com>
2025-10-23 12:53:38 -0500

    Update Caudalimetro.h

--- Caudalimetro.h
+++ Caudalimetro.h
@@ -1,27 +1,28 @@
 #pragma once
 #include <Arduino.h>
 
-struct caudalimetro {
-  portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-
+class caudalimetro {
+public:
   static constexpr float periodo_de_las_mediciones = 2000; // ms
   // 450 pulsos/L → 450000 pulsos/m^3. Resultado: m^3/s
   static constexpr float FLOW_CALIBRATION_FACTOR =
       450000.0f * (periodo_de_las_mediciones / 1000.0f);
 
-  static constexpr float ESCALA = 100.0f;
-
-  const byte pin;
+  static constexpr float ESCALA = 100.0f; // Se mantiene por ser constante de diseño
 
   caudalimetro(byte p);
-
-  uint32_t lastMillis = 0;
-  float    flowRate   = 0.0f;          // m^3/s (antes de ESCALA)
-  volatile uint32_t pulseCount = 0;
 
   void set_up();
   float reading();
 
   // ISR genérica: estática y con arg
   static void IRAM_ATTR isrThunk(void* arg);
+
+private:
+  portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+  const byte pin;
+
+  uint32_t lastMillis = 0;
+  float    flowRate   = 0.0f;          // m^3/s (antes de ESCALA)
+  volatile uint32_t pulseCount = 0;
 };
