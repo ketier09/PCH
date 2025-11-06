be757af62ff774e025c47585ddbc6da46f88beb9
ketier09 <kevinmojicac@gmail.com>
2025-11-05 22:35:24 -0500

    Update PantallaCustom.cpp

--- PantallaCustom.cpp
+++ PantallaCustom.cpp
@@ -7,52 +7,16 @@ PantallaCustom::PantallaCustom(uint8_t cs, uint8_t dc, uint8_t rst)
 
 
 void PantallaCustom::set_up() {
-  tft.begin();
-  tft.setRotation(3);
-  tft.fillScreen(ILI9341_BLACK);
-  tft.setFont(&FreeSansBold12pt7b);
-  dibujarBase();
-}
 
-void PantallaCustom::dibujarBase() {
-  tft.drawRGBBitmap(10, 10, imagenes[0].pixels, imagenes[0].width, imagenes[0].height);
-  tft.setTextColor(ILI9341_WHITE);
-  tft.setCursor(100, 30);
-  tft.print("MONITOREO PCH");
 }
 
 void PantallaCustom::actualizar(dato data[]) {
-  // Limpiar el área de datos
-  tft.fillRect(0, 60, 320, 180, ILI9341_BLACK); 
-
-  int y = START_Y; // Inicializar la posición Y
-
-  // LÍNEA 1: Cota Captación
-  dibujarDato(10, y,  data[cotaCaptacion].etiqueta, data[cotaCaptacion].valor, data[cotaCaptacion].unidad);
-  y += LINE_SPACING_Y;
-
-  // LÍNEA 2: Caudal Turbinable
-  dibujarDato(10, y, data[caudalTurbinable].etiqueta, data[caudalTurbinable].valor, data[caudalTurbinable].unidad);
-  y += LINE_SPACING_Y;
-
-  // LÍNEA 3: Cota Río
-  dibujarDato(10, y, data[cotaRio].etiqueta, data[cotaRio].valor, data[cotaRio].unidad);
-  y += LINE_SPACING_Y;
-
-  // LÍNEA 4: Generadores Activos
-  dibujarDato(10, y, data[cantidadGeneradoresActivos].etiqueta, data[cantidadGeneradoresActivos].valor, "");
+  
 }
 
-void PantallaCustom::dibujarDato(int x, int y, const char* etiqueta, float valor, const char* unidad) {
-  tft.setCursor(x, y);
-  tft.setTextColor(ILI9341_YELLOW);
-  tft.print(etiqueta);
-  tft.print(": ");
-  tft.setTextColor(ILI9341_GREEN);
-  tft.print(valor, 2);
-  tft.print(" ");
-  tft.print(unidad);
-
+void PantallaCustom::dibujar_imagen(uint8_t indice) {
+  tft.drawRGBBitmap(images[indice].x_index, images[indice].y_index, images[indice].pixels, images[indice].width, images[indice].height);
 }
+
 
 
