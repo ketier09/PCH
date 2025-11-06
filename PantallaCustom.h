a3db13248b2b783d70304322ea5d39ed02ecad3c
ketier09 <kevinmojicac@gmail.com>
2025-11-05 23:09:02 -0500

    Update PantallaCustom.h

--- PantallaCustom.h
+++ PantallaCustom.h
@@ -15,8 +15,19 @@ public:
 
 private:
   Adafruit_ILI9341 tft;
-  
+
+  static constexpr uint8_t NUM_ETIQUETAS = 4;
+  static constexpr unsigned long intervalo = 5000;
+  unsigned long tiempoAnterior = 0;
+  int indiceActual = 0;
+
+  float cotas[NUM_ETIQUETAS];
+  float flujos[NUM_ETIQUETAS];
+  const char* etiquetas[NUM_ETIQUETAS];
+
   void dibujar_imagen(uint8_t indice);
+  void mostrarDato(int x, int y, const char* etiqueta, float valor, const char* unidad);
 };
+
 
 
