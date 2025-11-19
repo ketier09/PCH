48a8b6b5c9de428538fafb46ddc31acfb6cda471
ketier09 <kevinmojicac@gmail.com>
2025-11-18 20:31:18 -0500

    Update Secrets.cpp

--- Secrets.cpp
+++ Secrets.cpp
@@ -1,6 +1,8 @@
+// Secrets.cpp
 #include "Secrets.h"
 
 const char key[]      = "";
-const char url[]      = "";
 const char email[]    = "";
 const char password[] = "";
+const char url[]      = ""; 
+const char projectId[] = "";
